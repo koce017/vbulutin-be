edade15e21177be7e2d2d4e33173c4b62ed3a280
koce017 <koce017@mail.com>
2025-03-20 13:57:43 +0100

    First commit

---
+++ src/main/java/com/koce017/vbulutin/VBulutinApplication.java
@@ -0,0 +1,13 @@
+package com.koce017.vbulutin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VBulutinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VBulutinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koce017/vbulutin/VBulutinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koce017.vbulutin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VBulutinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
